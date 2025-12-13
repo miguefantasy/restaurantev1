111e4e4eab01d9df3dfd13607d71347e52699ceb
Miguel Barroso <miguebd01@gmail.com>
2025-12-13 20:19:31 +0100

    Cambio de tipos en el Context.ts

--- context/Context.ts
+++ context/Context.ts
@@ -1,9 +1,12 @@
 import { create } from 'zustand'
-import { Plato } from '../entities/Plato'
 import { Cuenta } from '../entities/Cuenta'
+import { Menu } from '../entities/Menu'
+import { Plato } from '../entities/Plato'
 
 interface State {
-    menu: Plato[],
+    //menu es de tipo Menu, un tipo que hemos creado, Menu es un Array de Platos
+    menu: Menu,
+    //cuenta es de tipo Cuenta, un tipo que hemos creado y es tambien un Array de Platos
     cuenta: Cuenta,
     addPlatoAlMenu: (menu: Plato) => void,
     deletePlatoDelMenu: (id: string) => void,
@@ -14,14 +17,14 @@ interface State {
 export const useStateRestaurante = create<State>()((set) => ({
     menu: [],
     cuenta: [],
-    addPlatoAlMenu: (menu) => set((state) => ({
-        menu: [...state.menu, menu],
+    addPlatoAlMenu: (plato) => set((state) => ({
+        menu: [...state.menu, plato],
     })),
     deletePlatoDelMenu: (id) => set((state) => ({
         menu: state.menu.filter((item) => item.id !== id),
     })),
-    addPlatoMesa: (cuenta) => set((state) => ({
-        cuenta: [...state.cuenta, cuenta],
+    addPlatoMesa: (plato) => set((state) => ({
+        cuenta: [...state.cuenta, plato],
     })),
     deletePlatoMesa: (id) => set((state) => ({
         cuenta: state.cuenta.filter((item) => item.id !== id),
